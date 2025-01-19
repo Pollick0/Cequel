afe34852a0168fcfdd9af225e4b211fffa4475af
Pollick0 <salman.shakhawat@outlook.com>
2025-01-18 18:40:51 -0600

    Created the framework for insert/select commands

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
-{
+typedef struct {
     char *buffer;
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatemenType;
+
+typedef struct {
+    StatemenType type;
+} Statement;
+
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,
+                                Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }                          
+    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }          
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
 
 InputBuffer *new_input_buffer()
 {
@@ -23,16 +59,23 @@ InputBuffer *new_input_buffer()
 // Prints a prompt to the user. We do this before reading each line of input
 void print_prompt() { printf("db > "); }
 
-// getline() reads the line of input
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+            printf("This is where we would do an insert.\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("This is where we would do an select.\n");
+            break;
+    }
+}
 
-void read_input(InputBuffer *input_buffer)
-{
+
+void read_input(InputBuffer *input_buffer) {
     ssize_t bytes_read =
         getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-    if (bytes_read <= 0)
-    {
+    if (bytes_read <= 0) {
         printf("Error reading input\n");
         exit(EXIT_FAILURE);
     }
@@ -41,14 +84,6 @@ void read_input(InputBuffer *input_buffer)
     input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-// Frees memory from input_buffer
-void close_input_buffer(InputBuffer *input_buffer)
-{
-    free(input_buffer->buffer);
-    free(input_buffer);
-}
-
-
 int main(int argc, char *argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
@@ -56,12 +91,12 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (input_buffer->buffer[0] == ".") {
+        if (input_buffer->buffer[0] == '.') {
             switch (do_meta_command(input_buffer)) {
                 case (META_COMMAND_SUCCESS):
                     continue;
                 case (META_COMMAND_UNRECOGNIZED_COMMAND):
-                    printf("Unrecognizeds'. \n", input_buffer->buffer);
+                    printf("Unrecognized '%s'\n", input_buffer->buffer);
                     continue;
             }
         }
@@ -76,7 +111,7 @@ int main(int argc, char *argv[])
                 continue;
         }
 
-        execute_statement($statement);
+        execute_statement(&statement);
         printf("Executed.\n");
 
     }
